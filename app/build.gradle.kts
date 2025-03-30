cc0d577e16d2191d2736522743d5196981b017ba
Ihor Herasymenko <2904801+iherasymenko@users.noreply.github.com>
2025-03-29 21:07:00 -0400

    Fix org.liquibase:liquibase-core:4.31.1 metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,16 +8,17 @@ repositories {
 }
 
 dependencies {
-    /*
-     * This dependency causes problems with task app:moduleDescriptorRecommendations.
-     */
     implementation("org.liquibase:liquibase-core:4.31.1")
-
-    /*
-     * Enable this dependency and the java.activation module descriptor below because without those task
-     * app:moduleDescriptorRecommendations fails with "Module java.activation not found, required by java.xml.bind".
-     */
-//    implementation("com.sun.activation:javax.activation:1.2.0")
+    components {
+        withModule("org.liquibase:liquibase-core") {
+            allVariants {
+                withDependencies {
+                    removeIf { it.group == "javax.xml.bind" }
+                    add("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
+                }
+            }
+        }
+    }
 }
 
 java {
@@ -34,17 +35,4 @@ application {
 extraJavaModuleInfo {
     failOnMissingModuleInfo = false
     failOnAutomaticModules = false
-
-    /*
-     * Enable this module descriptor and the com.sun.activation:javax.activation dependency above because without those
-     * task app:moduleDescriptorRecommendations fails with "Module java.activation not found, required by java.xml.bind".
-     */
-//    module("com.sun.activation:javax.activation", "java.activation") {
-//        closeModule()
-//        requiresTransitive("java.datatransfer")
-//        requiresTransitive("java.desktop")
-//        requires("java.logging")
-//        exports("com.sun.activation.registries")
-//        exports("com.sun.activation.viewers")
-//    }
 }
